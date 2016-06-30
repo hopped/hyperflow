ba387dcf44bd44becad80129f1c9fe5d04027a32
Dennis Hoppe <hopped@users.noreply.github.com>
2016-06-30 15:33:48 +0200

    Update amqpCommand.config.js

--- functions/amqpCommand.config.js
+++ functions/amqpCommand.config.js
@@ -9,9 +9,9 @@ exports.options = {};
 exports.options.workdir = WORKDIR;
 
 if (S3_BUCKET !== undefined && (typeof S3_BUCKET === 'string' || S3_BUCKET instanceof String)) {
- exports.options.storage = 's3';/
- exports.options.bucket = 'paasage-bucket';
- exports.options.prefix = 'results';
+  exports.options.storage = 's3';
+  exports.options.bucket = S3_BUCKET;
+  exports.options.prefix = S3_PATH;
 } else {
   exports.options.storage = 'local';
 }
